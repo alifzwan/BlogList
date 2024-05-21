1b83524c27b3c60e0c5e7d96ffe70e54d1be4701
alifzwan <alifzakwan529@gmail.com>
2024-05-21 16:07:09 +0800

    refactor: Update config.js to handle different MongoDB URLs based on environment

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config() // The dotenv package is used to read environment variables from a .env file.
 
-const mongoUrl = process.env.mongoUrl
+const mongoUrl = process.env.NODE_ENV === 'test'
+    ? process.env.testMongoUrl
+    : process.env.mongoUrl
+
 const port = process.env.port
 
 module.exports = {
